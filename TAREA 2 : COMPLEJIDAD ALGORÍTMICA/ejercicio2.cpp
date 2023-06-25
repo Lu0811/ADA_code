63c27a80e016ede8df42cd16d7d37de50e44a503
Lu0811 <86023137+Lu0811@users.noreply.github.com>
2023-06-25 17:24:11 -0500

    Update ejercicio2.cpp

--- TAREA 2 : COMPLEJIDAD ALGORÍTMICA/ejercicio2.cpp
+++ TAREA 2 : COMPLEJIDAD ALGORÍTMICA/ejercicio2.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+void imprimir_cant_instrucciones(int N) {
+    int c = 0;
+
+    for(int i = 0; i <= N; i++) {
+        c++;
+        for(int j = i; j <= N; j++) {
+            c++;
+            cout<<i<<" , "<<j<<endl;
+            c++;
+        }
+        c++;
+    }
+    c++;
+
+    cout<<endl<<"Número total de instrucciones: "<<c<<endl;
+}
+int main() {
+    imprimir_cant_instrucciones(4);
+    return 0;
+}

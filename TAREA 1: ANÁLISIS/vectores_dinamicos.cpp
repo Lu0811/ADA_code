870017666605e0f52ff32ecefbd9caf08f3bd668
Lu0811 <86023137+Lu0811@users.noreply.github.com>
2023-06-25 17:37:05 -0500

    Update vectores_dinamicos.cpp

--- TAREA 1: ANÁLISIS/vectores_dinamicos.cpp
+++ TAREA 1: ANÁLISIS/vectores_dinamicos.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+// vector dinámico
+int *dynamicArray = nullptr;
+int elementCount = 0;
+int maxSize = 0;
+
+void resizeArray() {
+    const int delta = 10;
+    
+    int *tempArray, i;
+    tempArray = new int[maxSize + delta];
+    for (i = 0; i < maxSize; i++)
+        tempArray[i] = dynamicArray[i];
+    delete [] dynamicArray;
+    dynamicArray = tempArray;
+
+    maxSize += delta;
+}
+
+void insertElement(int elem) {
+    if (elementCount == maxSize)
+        resizeArray();
+    dynamicArray[elementCount++] = elem;
+}
+
+
+int main() {
+
+    insertElement(6);
+    insertElement(7);
+
+    for (int i = 0; i < elementCount; i++) {
+        cout << dynamicArray[i] << " ";
+    }
+
+    return 0;
+}
